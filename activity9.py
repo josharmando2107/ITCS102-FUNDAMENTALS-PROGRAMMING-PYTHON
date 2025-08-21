e230f7a3990efeaa16fbd5313829d85db7121d7a
josharmando2107 <armandojosh21@gmail.com>
2025-08-21 11:27:23 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,7 @@
+username = 'jo sh'
+password = 'sheesh'
+
+#print(username == 'josh')
+print((username == 'josh') and (password == 'sheesh'))
+print((username == 'josh') or (password == 'sheesh'))
+print(not((username == 'josh') or (password == 'sheesh')))
