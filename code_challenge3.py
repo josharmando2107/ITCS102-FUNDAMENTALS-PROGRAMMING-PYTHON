e563fc4c96b1e9ca61b5d66ef82a04be4c5d716b
josharmando2107 <armandojosh21@gmail.com>
2025-08-27 17:06:22 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,11 @@
+username = 'josh' 
+pwd = 'pracetamol'
+
+uname = input('input username')
+pasword = input('input password')
+
+if username == uname and pwd == pasword:
+               print("Access granted")
+
+else:           
+               print("Access denied")
